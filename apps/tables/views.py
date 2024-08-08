1f1197acf224d8ef80ad005801fc85f31fb24b93
Camille Lebrun <c_lebrun3@hetic.eu>
2024-08-08 16:38:06 +0200

    [CL] add filter for city hall

--- apps/tables/views.py
+++ apps/tables/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
-from web_project import TemplateLayout
+from django.utils.dateparse import parse_date
 from apps.volontaires.models import ContactForm
+from web_project import TemplateLayout
 
 class TableView(TemplateView):
     template_name = 'contact_form_list.html'
@@ -13,15 +14,34 @@ class TableView(TemplateView):
         sort_by = self.request.GET.get('sort', 'first_name')
         order = self.request.GET.get('order', 'asc')
 
-        # Determine the order (ascending or descending)
         if order == 'desc':
             sort_by = '-' + sort_by
 
-        # Query the database with the sorting parameters
-        context['contacts'] = ContactForm.objects.all().order_by(sort_by)
+        # Filter parameters
+        start_date = self.request.GET.get('start_date')
+        end_date = self.request.GET.get('end_date')
+        availability = self.request.GET.getlist('availability')
+
+        # Build the query
+        filters = {}
+        if start_date:
+            filters['start_date__gte'] = parse_date(start_date)
+        if end_date:
+            filters['end_date__lte'] = parse_date(end_date)
+        for day in availability:
+            if day in ContactForm._meta.get_fields():
+                filters[day] = True
+
+        # Query the database with the filtering and sorting parameters
+        context['contacts'] = ContactForm.objects.filter(**filters).order_by(sort_by)
         
         # Add the current sort parameters to the context
         context['current_sort'] = self.request.GET.get('sort', 'first_name')
         context['current_order'] = self.request.GET.get('order', 'asc')
+
+        # Add filter parameters to the context
+        context['current_start_date'] = start_date
+        context['current_end_date'] = end_date
+        context['current_availability'] = availability
         
         return context
